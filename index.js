b18bbd337b399b983c7f0bb6c98f81b525e3de8e
Haotian2006 <haotian2090@gmail.com>
2024-11-28 15:31:08 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,504 @@
+const grade = {
+  ip: "e122061d-1486-4459-a6a7-89f3019e0344", // In Progress
+  none: "0217b721-3915-475c-a17d-bd77f7a03e26", // None
+  pfp: "493192d3-1a52-4842-9bf3-5de159439ab9", // Pass/Fail Pass
+  pff: "10fed4c9-7878-44e7-a402-761fdb4dddc1", // Pass/Fail Fail
+  w: "6eaf6f6f-bba8-437d-8fd0-9bf96504fcf5", // Withdraw
+  "a+": "d3f84fe1-62ac-4eb8-a965-a8bfbfe3cb43",
+  a: "229ce560-fd41-4b14-9c14-1b9f67c463ff",
+  "a-": "7e0cb4cc-e163-4680-9715-dfb8040c1674",
+  "b+": "0d7ec1e4-b810-43c3-9bd8-a034461606c0",
+  b: "4b13340b-b178-4d6d-ba1a-d58d25d1794c",
+  "b-": "14105669-6042-4094-8865-b3f4bb50d469",
+  "c+": "3f4fb271-9d9f-4cd9-8780-acc3b0c0e8b7",
+  c: "b8bfc4de-b270-42a1-b5c7-50459a3a5943",
+  "c-": "0633b307-e9bb-4cbf-8a21-8b1befc71018",
+  "d+": "14e1e0fd-be48-40be-9148-cf33496f06de",
+  d: "3682181f-8387-4710-b62f-981215466b8c",
+  "d-": "7765e7e8-8c80-4ac7-b6c0-ac705d669c81",
+  f: "633d6ec3-70f7-4042-8305-271eb2d3143d",
+  110: "36bc9267-6cfa-4e07-8789-25b6ab65800e",
+  109: "1874f13f-e1b2-4d1e-92ce-f645055d5bbe",
+  108: "fbc601d7-abab-44c2-9567-12ff6b0c73a1",
+  107: "c7b1b67b-1ef7-4f3a-833b-fcafa56fb4c6",
+  106: "1adcf010-77fb-417b-8f29-5adc1f1694c8",
+  105: "6b038d7d-7446-49ba-9edd-0da7a4bee101",
+  104: "1a0ada23-6ecb-48f5-bb5d-19707c7a597d",
+  103: "32d0284a-d911-4061-9300-2a6823b93254",
+  102: "a2bf5d7e-5226-4268-96b6-d27f6930b53a",
+  101: "f095ed02-a5ec-4b04-9512-3d9cab331ec0",
+  100: "e42511da-97dc-4366-afda-56205f992883",
+  99: "438e7020-eb57-463c-b69a-9fd84fcd0b46",
+  98: "b7dd6345-bb65-46b2-8b5d-c3df439de873",
+  97: "2eea44bf-11d3-4b8c-924b-c393754a31e2",
+  96: "365674d5-996d-450f-b1b9-f8a039ecec7f",
+  95: "fa7c4621-fa9d-4742-9159-8485c9f55fe7",
+  94: "05990ef0-ad9e-4d5d-a460-a07cd55b4ba8",
+  93: "c33f7389-b938-4c68-b5d7-940f93de2f07",
+  92: "2014a1c7-ffe0-4833-a427-e6eb7886561b",
+  91: "48cea075-ced1-48d1-bc96-bdb0132d359f",
+  90: "00b1707e-f76a-4ed3-97f5-40f5b6379740",
+  89: "9154c77e-eab7-4048-bf9c-26a0113c24a0",
+  88: "470f105e-c026-4d95-816f-457f0e8ef2da",
+  87: "cfb218d6-3264-43a2-a191-ab51a4759045",
+  86: "7a183010-15fa-40ee-93a4-e863adb70f58",
+  85: "ceb3adc3-fc81-410a-a6d1-a88829a58723",
+  84: "eb32ffb6-9f81-4958-b919-1181329a6535",
+  83: "cff9b218-9889-4cc2-b122-f726ff9511e1",
+  82: "83b46f6c-fd18-4423-ba0e-a7a2331d32cc",
+  81: "3e3d8738-1189-44af-b73f-db41f0f4ae2e",
+  80: "a15399f8-0391-4d90-bc0a-420a12afad1d",
+  79: "32e3b9ea-04dd-4286-8fa9-3efcb59402d8",
+  78: "943040f6-1eb1-43e7-a9e7-aa3d2a30bca1",
+  77: "32c177dc-04c0-434e-bd7f-ada57a08ce36",
+  76: "3dc0d9e6-1e85-42c3-8405-e625f5231b25",
+  75: "dde4247b-c074-4ade-ad1c-7f3dee1d3212",
+  74: "505fa491-ba82-4961-bf69-47528db9723c",
+  73: "8ebdaede-e7c3-41c5-95c3-240ebc59a2a5",
+  72: "4285246a-bc75-48b7-ac54-c41dc8da81c0",
+  71: "9e785c6c-94a3-495f-9cdb-f9fb2def20f7",
+  70: "81fa3351-52d4-4a14-b558-8340a092be64",
+  69: "75021f0e-dc71-4239-9eb7-ab2c71c4fc61",
+  68: "69d6ced0-f842-4b00-81f0-0dc6ba8adcec",
+  67: "02c95401-0388-4a8c-bf80-87beb040509b",
+  66: "344d651b-4603-4384-9ead-0f1f7d332136",
+  65: "7f211049-80fb-4a40-a9fc-23bb89fd9e3f",
+  64: "f7f09793-1362-4b26-82a1-d38d6965a4a9",
+  63: "89696eb7-bfc6-46e0-b047-1a5237ee163b",
+  62: "b56fd66f-26a5-4eb7-b20f-8926673393c6",
+  61: "cda8e3dc-d23a-436f-8eed-e86bf4352990",
+  60: "36cafec2-6ef3-4790-89b9-cbcd8ea083a9",
+  59: "52521dca-8f8e-43a7-bf1b-f3455d527826",
+  58: "b02722df-ea57-410c-9355-4ac957e179b4",
+  57: "e39f2a09-b37f-4174-8640-20d73e8525d2",
+  56: "28835f2d-9821-45e5-8857-d13251dbd314",
+  55: "6c2dc9bb-ab52-4f57-ab8e-77db6ab5c4ff",
+  54: "8fed5d7c-bf19-45dd-91b5-7d747942a33f",
+  53: "4bef0e0b-e059-4f55-9633-76faf2a22319",
+  52: "5de997c1-9e0e-4523-a058-94a56107ab11",
+  51: "55de3cea-a330-4ad9-a2f5-cd379ee95bf7",
+  50: "caae7576-5c4a-4b56-8c07-20592ece4375",
+  49: "34a4dd80-4304-4ec2-9efa-2585667cde60",
+  48: "0f6d9019-9cb4-4cd1-a96b-fd1ea7a2bf3d",
+  47: "3c6a17ae-37b3-45b5-84a0-b0fafd17bd71",
+  46: "7b779241-f5ec-457e-8c37-f2fb42a64566",
+  45: "4d052ec1-23ca-4c33-8a94-0fdb4558502d",
+  44: "17170692-1d31-4b9d-bb83-8e1ff96f2356",
+  43: "3f3c8c3c-82ec-4a7a-8b75-921a31fadfca",
+  42: "1d78a948-6392-4b07-88f8-b6989632cb6e",
+  41: "757e55cb-9d59-4a99-8f79-f8c7fe717d36",
+  40: "9b6b7d72-e155-48fc-a252-19b34ae48090",
+  39: "fcafeda5-610e-402c-bcc0-90ce01070f7f",
+  38: "69a22c4b-2762-43da-b292-d7528fffd87e",
+  37: "9ea2d7fb-6bb5-4e75-928e-204c14f5b4eb",
+  36: "6f8c8031-eea4-4ef0-9b13-a94629b5ad08",
+  35: "49f5baf3-c330-483c-b566-8c5138a58175",
+  34: "45f0814c-9716-4823-914a-19719719999d",
+  33: "184112b1-373a-4c5e-91be-25dad217984b",
+  32: "6a678c59-ad09-434a-a893-0a1765928b58",
+  31: "930cb02b-88de-48f1-99dc-ccd869b31151",
+  30: "98954dc4-6d2c-413c-ae25-c89f09f0b9b9",
+  29: "48d6edd6-1a7b-4a1b-aa95-4ffdec105a80",
+  28: "111ecfc7-312f-4db3-8a67-675d4e320eca",
+  27: "079f7ea3-cef9-4ac2-aedd-b6d1b8bd6597",
+  26: "0f2f866a-161c-454a-9137-ce6655bab819",
+  25: "00fe858d-0086-4733-bc2e-d0176537b221",
+  24: "0be86621-054f-4429-957d-3f86957eb422",
+  23: "86631dbf-0d27-4672-99b6-fa7f1b017507",
+  22: "c9de67e4-ac78-475e-95b9-48e9287c7026",
+  21: "de818311-2a53-49ac-a577-70e676fe21f5",
+  20: "d3bfe691-199d-4a58-9abb-fef20744ac3b",
+  19: "429b3f56-9918-42d7-96c1-2bcdc35083b5",
+  18: "1a46b988-50d7-4b89-bb47-2f23a0b6aa67",
+  17: "b4c55e05-7d2b-4ea4-9058-eed4998ad164",
+  16: "4b9f4f78-dcea-459b-9a20-f7f114692170",
+  15: "3f12a25c-1a32-41d2-924a-87f2ec3259e7",
+  14: "eb39b612-c744-4359-89e5-5a3100d4fc69",
+  13: "53d0c34e-1b40-4eb8-acd1-5e50e12607f2",
+  12: "c3f61aa6-31ee-4632-8780-95e59df3ba12",
+  11: "ba550b9b-a7cc-42ae-ac08-94c22309b7b6",
+  10: "0d34102d-3f39-4a6b-89a2-8940d858fbf5",
+  9: "dd7d8544-b691-4a9c-8917-fb9fd7ea1738",
+  8: "12693f98-4df4-4a72-874b-ba8b697542c0",
+  7: "fb8fa504-7d44-435f-a2a8-2a1872c8070e",
+  6: "0c8bb801-f234-4f35-8123-c41219fd70ce",
+  5: "9672be8f-1863-4997-86e2-9a1347e3137a",
+  4: "5616138c-01cc-41ce-8b81-da5236ff675c",
+  3: "52531eef-57b8-44bb-9418-3fdbb2dd1f3b",
+  2: "873f6f0b-60c7-4ffe-a82f-c0cae27b29e5",
+  1: "869d2029-46c1-4129-aebe-873690b1a8ab",
+  0: "bdc783d7-a24c-4997-acf0-a7ede496e392",
+};
+const gradeLevelUUID = "form_6d343b98-1791-4946-a37d-db3752ce3c49";
+const subjectTypeUUID = "form_624246fd-3add-4b77-9576-c7093ee36fe4";
+const subjectTypeOtherUUID = "form_c3c15020-9f4e-49ce-8e82-d3072054fbd7";
+const courseLevelUUID = "form_5d9c4382-449e-4551-9c4c-e52366015ee4";
+const gradeScaleUUID = "form_7375809c-9f5a-45ca-a025-c9e4ecfdc293";
+const courseNameUUID = "form_3ecf3c52-c90e-4efb-9b12-103c0704a421";
+const monthUUID = "form_76ed993f-fa70-4d47-9c70-f0816807be5f_m";
+const yearUUID = "form_76ed993f-fa70-4d47-9c70-f0816807be5f_y";
+const creditsUUID = "form_fc8e4be9-b8ef-4b00-ae13-ce4e072c4e15";
+const noCreditsUUID = "form_6ee69336-f31c-4f5c-87a4-23dc1d74cf76";
+const gradePrimaryUUID = "form_acdb7194-1273-459e-9d61-22b12f1782d0";
+const gradeNumberUUID = "form_7540cbd0-f278-4ed5-ba2d-081928d9eb0c";
+const gradeOtherUUID = "form_152f8b15-7704-442d-86a5-3967c97d1b34";
+const schoolNameUUID = "form_75b1dc98-6f59-47f4-8de0-9e0b08d4ab5f";
+
+
+const gradeLevel = {
+  9: "77a4046a-65df-4358-9689-3c30549b4c24",
+  10: "739f4ea0-a88c-4b98-b525-5cdcb5c11a65",
+  11: "c04a613c-ba37-4eb9-8ce3-002fdb1a6027",
+  12: "ec3517bc-0626-488b-aad8-c7f510814460",
+  13: "0084488f-edbc-4f14-8d3d-7a402b1189b0",
+};
+
+const subject = {
+  art: "012a3bf5-6169-4f71-85c5-5b7c58befc93",
+  cs: "33307535-a89d-4a81-9fdd-ca8a022958a7",
+  english: "3dc18902-d8b5-4c58-a78a-c743df93d817",
+  foreign: "292c25d0-be28-4f7e-96f1-fc016d11f774",
+  history: "96d2992c-afe7-4279-9d0f-71ec08d95559",
+  "independent study": "19c02060-9210-4b31-952c-219cc44a5259",
+  math: "6e501801-c68a-4827-8a2c-4669c5019011",
+  religion: "39719196-2e4c-48f4-9285-9e2ea6e044cb",
+  science: "429d1332-0af1-4476-8270-f247e205c954",
+  elective: "4771b04a-7d8b-4955-9f09-3862c9a55853",
+  pe: "6219930d-4105-47af-9218-eb310bdb774f",
+  other: "873f4aa0-88fb-4b10-8895-66b5b7bf3075",
+};
+
+const courseLevel = {
+  na: "fa7ec536-7b60-4720-b9f1-3d1cc9019841",
+  regular: "db65d1bc-f9c8-40ea-a1c0-e91cd83f6101",
+  "college prep": "61bd436f-1346-4a7b-aac5-3fb0f4a5b5b7",
+  accelerated: "c09b050e-be57-4711-b47b-7fca1ef586ba",
+  advanced: "67804daf-1fe4-47aa-be61-9a1954518b3b",
+  honors: "e824bb9a-2889-47a7-9ff4-cf570affddc1",
+  "high honors": "52eaf8da-2f5c-4e83-b914-883593d40a79",
+  "early college experience": "d7124794-5d7b-42de-aa8c-ff7464445874",
+  "dual enrollment": "d64f75db-dccb-4fcb-8e01-2a3c8d28e9ad",
+  "college credit": "5d3f8724-0b4a-407d-ae35-3fcff472bdc6",
+  "pre-ap": "3ba32dfe-0fbf-4ecf-bb88-c552117969e9",
+  ap: "3b186178-27aa-40cc-8aaa-400bda0c3983",
+  "post-ap": "175adbf9-8991-4abc-983c-e4b7e02b02c4",
+  "pre-ib": "181c8bdf-0a52-4552-8b6b-6c7c06c23cd4",
+  "ib (standard/sl)": "703ab981-8147-4be4-ab0a-40823a21d346",
+  "ib (higher/hl)": "e49f8d9e-3a4a-4a56-8fa1-f086c2dd0642",
+  "ib no level (tok, research)": "6be6a0f9-e5af-431a-b035-1471250e80a2",
+  "ib (predicted)": "e54068ed-8f62-41c3-a350-58259fdd6e9b",
+  "pre-aice": "69e64fab-355b-4bad-bc1d-292928aa0027",
+  aice: "86f61330-9606-4dbc-9acc-cb25db751200",
+  "igcse/gcse ordinary level": "10df73c9-0baa-4b3a-8ce9-5fc6a465cebb",
+  "gce advanced level": "3783c164-093c-4eab-be0b-18e5a394d721",
+  "gce advanced subsidiary level": "74dfcca1-cdbe-478c-9628-5ee8a5f29a5f",
+};
+
+const month = {
+  january: "01",
+  february: "02",
+  march: "03",
+  april: "04",
+  may: "05",
+  june: "06",
+  july: "07",
+  august: "08",
+  september: "09",
+  october: "10",
+  november: "11",
+  december: "12",
+};
+
+const gradeScale = {
+  letter: "4130f08a-4c0d-4f12-b259-eb77817f7045",
+  "number 1-100": "ac272139-2804-40cf-ab25-cc987968fe72",
+  "number 1-110": "3a243602-a1a9-4671-9632-82e271e2e48e",
+  "0.0 - 4.0": "619a56d5-ffbd-41ea-b3f1-120770d83ec9",
+  "0.0 - 5.0": "f64ff9dd-711f-430e-8aa6-806667b1205d",
+  "number 1-7": "291d74d5-ec14-497a-b06f-b0173470f281",
+  "number 1-10": "f8e950b2-80b0-4f1b-bc03-0f8129ab0517",
+  "number 1-11": "e62d49c7-f80c-4e93-8e43-d1a962f13c52",
+  "number 1-20": "2fee1163-a017-4481-8143-302096a425e8",
+  narrative: "fe72e6a4-21aa-4a61-b13d-272f43001a67",
+  "other/scale not listed": "f050bb97-bd4f-4723-8027-1bf224f3b160",
+};
+
+const formData = {
+  gradeLevel: {
+    formID: gradeLevelUUID,
+    options: gradeLevel,
+  },
+  startMonth: {
+    formID: monthUUID,
+    options: month,
+  },
+  startYear: {
+    formID: yearUUID,
+  },
+  subjectArea: {
+    formID: subjectTypeUUID,
+    options: subject,
+  },
+  courseName: {
+    formID: courseNameUUID,
+  },
+  courseLevel: {
+    formID: courseLevelUUID,
+    options: courseLevel,
+  },
+  gradeScale: {
+    formID: gradeScaleUUID,
+    options: gradeScale,
+  },
+  grade: {
+    formID: gradePrimaryUUID,
+    options: grade,
+  },
+  courseCredits: {
+    formID: creditsUUID,
+  },
+  subjectTypeOther: {
+    formID: subjectTypeOtherUUID,
+  },
+};
+
+const { error } = require("console");
+const fs = require("fs");
+const path = require("path");
+
+const networkDumpFilePath = path.join(__dirname, "networkDump.txt");
+const courseFilePath = path.join(__dirname, "courses.txt");
+
+const networkDump = fs.readFileSync(networkDumpFilePath, "utf8");
+const courses = fs.readFileSync(courseFilePath, "utf8");
+
+const matchUrl = networkDump.match(/"([^"]*)"/);
+const url = matchUrl ? matchUrl[1] : null;
+
+const headersMatch = networkDump.match(/"headers":\s*({[^}]*})/);
+const headersString = headersMatch ? headersMatch[1] : null;
+const headersObject = JSON.parse(headersString);
+
+const contentType = headersObject["content-type"];
+const boundaryMatch = contentType.match(/boundary=(.*)/);
+const boundary = '--' + (boundaryMatch ? boundaryMatch[1] : null);
+
+const bodyMatch = networkDump.match(/"body":\s*"((?:[^"\\]|\\.)*)"/s);
+const bodyStr = bodyMatch ? bodyMatch[1] : null;
+
+function parseTable(input) {
+  const rows = input.trim().split("\n");
+
+  const headers = [
+    "gradeLevel",
+    "startMonth",
+    "startYear",
+    "subjectArea",
+    "courseName",
+    "courseLevel",
+    "gradeScale",
+    "grade",
+    "courseCredits",
+    'otherCourseName',
+  ];
+ 
+  return rows.map((row) => {
+    const values = row.split("\t").map((value) => value.trim());
+    const obj = {};
+
+    headers.forEach((header, index) => {
+      obj[header] = values[index] || null;
+    });
+    return obj;
+  });
+}
+
+const coursesData = parseTable(courses);
+
+const bodyMatchComp =
+  /Content-Disposition: form-data; name=\\"([^"]+\\)"\\r\\n\\r\\n([^\\]*)/g;
+
+const bodyStart = [];
+let match;
+let iters = 1;
+while ((match = bodyMatchComp.exec(bodyStr)) !== null && iters < 6) {
+  iters++;
+  bodyStart.push({
+    name: match[1].replace(/\\$/, ""),
+    value: match[2].trim(),
+  });
+}
+
+function createBody(idx) {
+  const entry = coursesData[idx];
+  const body = bodyStart.slice();
+  for (const key in entry) {
+    if (key === "otherCourseName") {
+        continue;
+    }
+    if( key == 'grade' ){
+        const gradeScale = entry['gradeScale'];
+        if (gradeScale == 'narrative') {
+
+        }else if(gradeScale == '0.0 - 4.0' ){
+            const gradeNumber = parseFloat(entry[key]);
+            if(gradeNumber < 0 || gradeNumber > 4 || isNaN(gradeNumber)){
+                throw new Error(`grade number ${entry[key]} is out of range for ${key} at ${idx}`);
+            }
+
+            body.push({
+                name: gradeNumberUUID,
+                value: gradeNumber,
+            });
+        }else if(gradeScale == '0.0 - 5.0' ){
+            const gradeNumber = parseFloat(entry[key]);
+            if(gradeNumber < 0 || gradeNumber > 5 || isNaN(gradeNumber)){
+                throw new Error(`grade number ${entry[key]} is out of range for ${key} at ${idx}`);
+            }
+            body.push({
+                name: gradeNumberUUID,
+                value: gradeNumber,
+            });
+        }else if (gradeScale == 'other/scale not listed'){
+            body.push({
+                name: gradeOtherUUID,
+                value: entry[key],
+            });
+        }else if (gradeScale == 'letter'){
+            const gradeID = grade[entry[key].toLowerCase()];
+            if (gradeID) {
+                body.push({
+                  name: gradePrimaryUUID,
+                  value: gradeID,
+                });
+              }else{
+                throw new Error(`unknown grade ${entry[key]} for ${key} at ${idx}`);
+              }
+        }else{
+            const gradeNumber = parseInt(entry[key]);
+            if(isNaN(gradeNumber) || !grade[gradeNumber.toString()]){
+                throw new Error(`grade number ${entry[key]} is out of range for ${key} at ${idx}`);
+            }
+            
+            const gradeID = grade[gradeNumber.toString()]; 
+            if (gradeID) {
+                body.push({ 
+                  name: gradePrimaryUUID,
+                  value: gradeID,
+                });
+              }else{
+                throw new Error(`unknown grade ${entry[key]} for ${key} at ${idx}`);
+              }
+        }
+        continue;
+    }else if(key == 'subjectArea' && entry[key] == 'other'){
+        const otherData = entry['otherCourseName'];
+        if(otherData == null){
+            throw new Error(`missing other course name for ${key} at ${idx}`);
+        }
+        body.push({
+            name: subjectTypeOtherUUID,
+            value: otherData,
+        });
+    }else if (key == 'courseCredits'){
+        if (entry[key] == 'none'){
+            body.push({
+                name: noCreditsUUID,
+                value: "1",
+            });
+        }else{
+            if (isNaN(parseFloat(entry[key])) || parseFloat(entry[key]) <= 0) {
+                error(`Invalid value for ${key}: ${entry[key]} at ${idx}`);
+            }
+            body.push({
+                name: creditsUUID,
+                value: entry[key],
+            });
+        }
+        continue;
+    }
+    if (entry[key] === null ) {
+      throw new Error(`Missing value for ${key} at ${idx}`);
+    }
+    const form = formData[key];
+    if (form.options) {
+ 
+      const option = form.options[entry[key]];
+      if (option) {
+        body.push({
+          name: form.formID,
+          value: option,
+        });
+      }else{
+        throw new Error(`unknown option ${entry[key]} for ${key} at ${idx}`);
+      }
+    } else {
+      body.push({
+        name: form.formID,
+        value: entry[key],
+      });
+    }
+  }
+
+  body.push({
+    name : "form_28338f1d-d514-4ee3-9c00-a427d672692e",
+    value : "",
+  });
+
+  body.push({
+    name : "form_b68d482e-65f8-4de3-9318-ec13e52d975a",
+    value : "",
+  });
+
+  body.push({
+    name : "form_62989f4f-e200-473b-acb2-8788b5bc62c2",
+    value : "0",
+  });
+
+  body.push({
+    name: "cmd", 
+    value: "submit",
+  });
+
+  return body;
+}
+
+function createBodyString(body){
+    return body.map((entry) => {
+        return `${boundary}\r\nContent-Disposition: form-data; name=\"${entry.name}\"\r\n\r\n${entry.value}`;
+    }).join("\r\n") + "\r\n"+ boundary+"--\r\n"; 
+}
+
+async function sendRequest(bodyStr){
+    await fetch(url, {
+        method: "POST",
+        headers: headersObject,
+        body: bodyStr, 
+    }).then()
+    // console.log(url)
+    // console.log(headersObject)
+    // console.log(bodyStr)
+}
+
+function sendCourse(idx){
+    const body = createBody(idx);
+    const bodyStr = createBodyString(body);
+    sendRequest(bodyStr);
+}
+
+async function sendCourses(){
+    const parsed = []
+    for(let i = 0; i < coursesData.length; i++){
+        const body = createBody(i)
+        parsed.push(createBodyString(body));
+    }
+
+    for(let i = 0; i < parsed.length; i++){
+        await sendRequest(parsed[i]);
+        await new Promise(resolve => setTimeout(resolve, 500)); 
+    }
+    console.log("done")
+}
+
+sendCourses()
+ 
